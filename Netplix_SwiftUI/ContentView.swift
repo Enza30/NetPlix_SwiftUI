8b04c72b30c5d414bade8b3cccc76d47196a7e40
Farendza Muda <farendza.muda@gmail.com>
2022-11-23 23:13:49 +0700

    Initial Commit

---
+++ Netplix_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Netplix_SwiftUI
+//
+//  Created by Farendza Muda on 23/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Netplix_SwiftUI/Netplix_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netplix_SwiftUIApp.swift
+//  Netplix_SwiftUI
+//
+//  Created by Farendza Muda on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Netplix_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
